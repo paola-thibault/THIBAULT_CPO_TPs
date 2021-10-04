055fbf3fcb2822788427fe93ea907b004d96a9a2
paola <paola@192.168.114.245>
2021-10-04 16:36:52 +0200

    Création du dépot n°2

---
+++ TP1/src/manipNombresInt_THIBAULT.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author paola
+ */
+
+import java.util.Scanner;
+
+public class manipNombresInt_THIBAULT {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nb1 =0;
+                
+        System.out.println("Saisissez 2 entiers :");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entier n°1 : ");
+         nb1=sc.nextInt();
+    
+        System.out.println("Entier n°2 : ");
+        int nb2=sc.nextInt();
+
+    }
+}
