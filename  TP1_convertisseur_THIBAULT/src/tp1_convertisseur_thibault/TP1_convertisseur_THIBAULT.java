731856fafad5f2ca53e182cb63b6adedc483c0d0
paola <paola@192.168.114.245>
2021-10-08 18:58:55 +0200

    Avancement 08/10/21 19h00

---  TP1_convertisseur_THIBAULT/src/tp1_convertisseur_thibault/TP1_convertisseur_THIBAULT.java
+++  TP1_convertisseur_THIBAULT/src/tp1_convertisseur_thibault/TP1_convertisseur_THIBAULT.java
@@ -16,23 +16,89 @@ public class TP1_convertisseur_THIBAULT {
      * @param args the command line arguments
      */
     
+    //1
     public static double CelciusVersKelvin ( double tempCelcius) {
         double tempKelvin = tempCelcius + 273;
         return tempKelvin;
      }
     
+    //2
+    public static double KelvinVersCelcius ( double tempKelvin) {
+        double tempCelcius = tempKelvin - 273;
+        return tempCelcius;
+     }    
+    
+    
+    //3
+    public static double FarenheitVersCelcius ( double tempFarenheit) {
+        double tempCelcius = tempFarenheit + 32;
+        return tempCelcius;
+     }
+    
+    //4
+    public static double CelciusVersFarenheit ( double tempCelcius) {
+        double tempFarenheit = tempCelcius - 32;
+        return tempFarenheit;
+     }
+    
+    //5
+    public static double KelvinVersFarenheit ( double tempKelvin) {
+        double tempFarenheit = tempKelvin * 1.8 - 460;
+        return tempFarenheit;
+     }    
+
+    //6
+    public static double FarenheitVersKelvin ( double tempFarenheit) {
+        double tempKelvin = (tempFarenheit + 459.67) / 1.8;
+        return tempKelvin;
+     }
+    
+    //MAIN
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        double valeurC ;
+        double valeur ;
         System.out.println("saisir une valeur réelle : ");
-        valeurC =sc.nextDouble();
-        System.out.println(valeurC);
+        valeur =sc.nextDouble();
+        System.out.println(valeur);
         
-        System.out.println("Kelvin : ");
-        double valeurK = CelciusVersKelvin(valeurC);
-        System.out.println(CelciusVersKelvin(valeurK));
+        
+        
+        double valeurC =0;
+        double valeurK =0;
+        double valeurF =0;
+        
+        //1
+        System.out.println("Celcius -> Kelvin : ");
+        valeurK = CelciusVersKelvin(valeur);
+        System.out.println(valeurK);
 
+        //2
+        System.out.println("Kelvin -> Celcius : ");
+        valeurC = KelvinVersCelcius(valeur);
+        System.out.println(valeurC);
+              
+        //3
+        System.out.println("Farenheit -> Celcius : ");
+        valeurC = FarenheitVersCelcius(valeur);
+        System.out.println(valeurF);
+
+        //4
+        System.out.println("Celcius -> Farenheit : ");
+        valeurF = CelciusVersFarenheit(valeur);
+        System.out.println(valeurF);
+    
+        //5
+        System.out.println("Kelvin -> Farenheit : ");
+        valeurF = KelvinVersFarenheit(valeur);
+        System.out.println(valeurF);
+       
+        //6
+        System.out.println("Farenheit -> Kelvin : ");
+        valeurK = FarenheitVersKelvin(valeur);
+        System.out.println(valeurK);
+        
+        
     }
     
 }
