94e06fd270c4ef63b1e973ec8be5a29274d9494d
paola <paola@192.168.114.245>
2021-10-10 02:04:23 +0200

    Avancement 10/10/21

--- TP1_guessMyNumber_THIBAULT/src/tp1_guessmynumber_thibault/TP1_guessMyNumber_THIBAULT.java
+++ TP1_guessMyNumber_THIBAULT/src/tp1_guessmynumber_thibault/TP1_guessMyNumber_THIBAULT.java
@@ -1,8 +1,11 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+OBJECTIF : réaliser un jeu de type « Guess My Number » : 
+un nombre aléatoire est choisi, l’utilisateur doit le deviner.
+*/
 package tp1_guessmynumber_thibault;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +17,33 @@ public class TP1_guessMyNumber_THIBAULT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Random generateurAleat = new Random();
+        Scanner sc = new Scanner(System.in);
+        int nb_Alea;  
+        int nb_ut = 101;
+        int compteur = 0;
+        
+        nb_Alea = generateurAleat.nextInt(100);
+        while (nb_ut != nb_Alea) {
+            System.out.println("choisissez un nombre entre 0 et 100 :");
+            nb_ut = sc.nextInt();
+        
+            if (nb_ut == nb_Alea){
+                System.out.println("GAGNÉ");
+                System.out.println("compteur = " + compteur);
+                }
+
+            if (nb_ut > nb_Alea){
+                System.out.println("trop grand");
+                }
+
+            if (nb_ut < nb_Alea){
+                System.out.println("trop petit");
+                }
+            
+            compteur = compteur +1;
+        } 
+        }
     }
     
-}
+
