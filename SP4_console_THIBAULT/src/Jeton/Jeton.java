fe2368cc9118beab45319fb03a41e6585943b7b9
paola <paola@192.168.114.245>
2021-11-12 17:02:44 +0100

    Avancement 11/12/21

---
+++ SP4_console_THIBAULT/src/Jeton/Jeton.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Jeton;
+
+/**
+ *
+ * @author paola
+ */
+public class Jeton {
+    String couleur ;
+    
+    public void Jeton (String color) {
+        couleur = color ;
+    }
+    
+    public String lireCouleur (){
+        return couleur ;
+    } 
+    
+             
+}
