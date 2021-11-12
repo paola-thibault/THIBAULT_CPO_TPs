6892c9931e8f4981e4106f87579fe8d08b6ff72b
paola <paola@192.168.114.245>
2021-11-12 23:08:31 +0100

    Avancement 11/12/21

--- TP2_Bieres_THIBAULT/src/tp2_bieres_thibault/TP2_Bieres_THIBAULT.java
+++ TP2_Bieres_THIBAULT/src/tp2_bieres_thibault/TP2_Bieres_THIBAULT.java
@@ -14,7 +14,42 @@ public class TP2_Bieres_THIBAULT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /**
+        BouteilleBiere uneBiere = new BouteilleBiere() ;
+        uneBiere.nom = "Cuvée des trolls";
+        uneBiere.degreAlcool = 7.0 ;
+        uneBiere.brasserie = "Dubuisson";
+        uneBiere.lireEtiquette();
+        
+        BouteilleBiere uneBiere2 = new BouteilleBiere();
+        uneBiere2.nom = "leffe";
+        uneBiere2.degreAlcool = 6.6f;
+        uneBiere2.brasserie = "Abbaye de Leffe";
+        uneBiere2.lireEtiquette();
+        */
+        
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson") ;
+        uneBiere.lireEtiquette();
                 
+        BouteilleBiere uneBiere2 = new BouteilleBiere("Leffe", 6.6,"Abbaye de Leffe") ;
+        uneBiere2.lireEtiquette();
+        
+        BouteilleBiere uneBiere3 = new BouteilleBiere("1664", 5.5f, "Le Canon \n");
+        uneBiere3.lireEtiquette();
+        
+        BouteilleBiere uneBiere4 = new BouteilleBiere("Pelforth", 5.8f, "Mons-en-baroeuf \n");
+        uneBiere4.lireEtiquette();
+        
+        uneBiere4.Décapsuler();
+        uneBiere4.Décapsuler();
+        
+        //System.out.println(uneBiere4); 
+        
+        BouteilleBiere uneBiere5 = new BouteilleBiere("Barile White", 4.9f, "Baril sur Brest \n");
+        uneBiere5.lireEtiquette();
+        uneBiere5.Décapsuler();
+        uneBiere5.Décapsuler();
+        System.out.println(uneBiere5);
         
     }
     
